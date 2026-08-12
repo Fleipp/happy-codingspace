abfb4b94d3ba3bcaed08cf6acff5d859955b52bb
Fleipp <distefano3h@gmail.com>
2026-08-11 23:30:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,12 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
+    prerender: {
+      enabled: true,
+      autoStaticPathsDiscovery: true,
+      crawlLinks: true,
+      failOnError: true,
+    },
     server: { entry: "server" },
   },
 });
